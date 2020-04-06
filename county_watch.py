dbdf00e74dfd2fb375cf8c80b5c44490d98f343f
Brandon LaFleur <blafleur@umich.edu>
2020-04-06 18:38:58 -0400

    Updated for 4/6/20

--- county_watch.py
+++ county_watch.py
@@ -1,11 +1,16 @@
+from math import log
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from matplotlib.dates import SU
 import datetime
 
 
 class County():
     def __init__(self, county, state, style):
+
+        window = 7
 
         # Store name information.
         self.county = county
@@ -21,16 +26,30 @@ class County():
 
         # Get rolling average columns.
         self.data['roll_new_cases'] = \
-            self.data.new_cases.rolling(window=5).mean()
+            self.data.new_cases.rolling(window=window).mean()
         self.data['roll_cases'] = \
-            self.data.cases.rolling(window=5).mean()
+            self.data.cases.rolling(window=window).mean()
 
+        # Get doubling rate.
+        ln2 = [log(i, 2) for i in self.data['roll_cases']]
+        slope_list = [0]*window
+        last = ln2[window-1]
+        for ele in ln2[window:]:
+            try:
+                slope_list.append(1/(ele - last))
+                last = ele
+            except ZeroDivisionError:
+                slope_list.append(0)
+
+        slope_list = np.array(slope_list)
+        slope_list[slope_list==0] = 100
+        self.data['doubling_rate'] = slope_list
 
 def plot_time_series(filename, counties):
 
-    fig, ax = plt.subplots(figsize=(10, 5), nrows=1, ncols=2)
+    fig, ax = plt.subplots(figsize=(14, 5), nrows=1, ncols=3)
     date_fmt = mdates.DateFormatter('%Y-%m-%d')
-    locator= mdates.MonthLocator()
+    locator= mdates.WeekdayLocator(byweekday=SU, interval=1)
 
     # Plot the time series data.
     for county in counties:
@@ -41,6 +60,9 @@ def plot_time_series(filename, counties):
                    label=county.county)
         ax[1].loglog(county.data['roll_cases'], county.data['roll_new_cases'],
                 county.style, label=county.county)
+        ax[2].plot(xaxis, county.data['doubling_rate'],
+                county.style, label=county.county)
+
     ax[0].set_xlabel('Date')
     ax[0].set_ylabel('Total Cases')
     ax[0].legend()
@@ -50,6 +72,13 @@ def plot_time_series(filename, counties):
     ax[1].set_xlabel('Total Cases')
     ax[1].set_ylabel('New Cases')
     ax[1].legend()
+
+    ax[2].set_xlabel('Date')
+    ax[2].set_ylabel('Doubling Rate (doubles every X days)')
+    ax[2].set_ylim((0, 14))
+    ax[2].xaxis.set_major_locator(locator)
+    ax[2].invert_yaxis()
+    ax[2].legend()
 
     fig.autofmt_xdate()
     fig.savefig(f'{filename}')
